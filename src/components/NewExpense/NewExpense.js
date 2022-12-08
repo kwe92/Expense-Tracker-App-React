492fa94e4d442704558d019e513c01988dd9449c
Kweayon  Clark <Kweayon@gmail.com>
2022-12-07 18:48:28 -0600

    Update
    Recieve data from a child component via callback

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,9 +2,14 @@ import React from "react";
 import NewExpenseForm from "./NewExpenseForm";
 import "./NewExpense.css";
 
+const newExpenseDataHandler = (enteredExpenseData) => {
+  const expenseData = enteredExpenseData;
+  console.log(expenseData);
+};
+
 const NewExpense = () => (
   <div className="new-expense">
-    <NewExpenseForm />
+    <NewExpenseForm onSubmit={newExpenseDataHandler} />
   </div>
 );
 
