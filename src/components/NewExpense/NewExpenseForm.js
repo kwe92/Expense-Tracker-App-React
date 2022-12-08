9afe6578a924df5e68939c54c934e9e7968ed432
Kweayon  Clark <Kweayon@gmail.com>
2022-12-07 18:02:12 -0600

    Refactor

--- src/components/NewExpense/NewExpenseForm.js
+++ src/components/NewExpense/NewExpenseForm.js
@@ -2,13 +2,19 @@ import React from "react";
 import FormHandlers from "./FormHandlers";
 import { useState } from "react";
 import "./NewExpenseForm.css";
+import ExpenseItemModel from "../models/ExpenseItemModel";
+
+const _rand = () => Math.floor(Math.random() * 9999 + 1000);
 
 const NewExpenseForm = (props) => {
-  const inputObj = {
-    title: "",
-    amount: "",
-    date: "",
-  };
+  const inputObj = new ExpenseItemModel(_rand(), "", "", "").toObject();
+
+  // {
+  //   id: some_random_number
+  //   title: "",
+  //   amount: "",
+  //   date: "",
+  // };
   const [userInput, setUserInput] = useState(inputObj);
   console.log("On Render", userInput);
   return (
