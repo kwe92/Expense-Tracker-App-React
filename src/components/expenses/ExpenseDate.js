58576ee1fdca031a6253c5033692b40b3d0dc704
Kweayon  Clark <Kweayon@gmail.com>
2022-12-09 01:01:05 -0600

    Update

--- src/components/expenses/ExpenseDate.js
+++ src/components/expenses/ExpenseDate.js
@@ -1,5 +1,6 @@
 import "./ExpenseDate.css";
 const ExpenseDate = (props) => {
+  // console.log("FROM EXPENSE DATE JS".props.date);
   const year = props.date.getFullYear();
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
