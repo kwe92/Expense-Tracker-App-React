f3ab209f1374c90c34c36097257a49c8a82428d0
Kweayon  Clark <Kweayon@gmail.com>
2022-12-09 01:00:52 -0600

    Update

--- src/components/data/ExpenseData.js
+++ src/components/data/ExpenseData.js
@@ -1,41 +1,11 @@
 import ExpenseItemModel from "../models/ExpenseItemModel";
 
 // Hard coded data to populate the UI
-const fetchExpenses = () => {
-  const expenses = [
-    new ExpenseItemModel(1001, "Coffee", 12.46, new Date("2022", "11", "04")),
-    new ExpenseItemModel(1002, "Food", 13.46, new Date("2022", "11", "04")),
-    new ExpenseItemModel(1003, "socks", 8.46, new Date("2022", "11", "06")),
-    new ExpenseItemModel(1004, "misc", 25.46, new Date("2022", "11", "09")),
-  ];
-  return expenses;
-};
+const expenseData = [
+  new ExpenseItemModel(1001, "Coffee", 12.46, new Date("2022", "11", "04")),
+  new ExpenseItemModel(1002, "Food", 13.46, new Date("2022", "11", "04")),
+  new ExpenseItemModel(1003, "socks", 8.46, new Date("2022", "11", "06")),
+  new ExpenseItemModel(1004, "misc", 25.46, new Date("2022", "11", "09")),
+];
 
-export default fetchExpenses;
-
-// const expenses = [
-//   {
-// id: 1001,
-// title: "Coffee",
-// price: 12.46,
-// date: new Date("2022", "11", "04"),
-//   },
-//   {
-// id: 1002,
-// title: "Food",
-// price: 13.46,
-// date: new Date("2022", "11", "04"),
-//   },
-//   {
-// id: 1003,
-// title: "socks",
-// price: 8.46,
-// date: new Date("2022", "11", "06"),
-//   },
-//   {
-// id: 1004,
-// title: "misc",
-// price: 25.46,
-// date: new Date("2022", "11", "09"),
-//   },
-// ];
+export default expenseData;
