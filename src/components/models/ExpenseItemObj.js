bebcbef18affc2a200e6dc98ebf749d7b4220caf
Kweayon  Clark <Kweayon@gmail.com>
2022-12-09 21:36:55 -0600

    Added Entity Object
    Refactored the class expense item into a function

---
+++ src/components/models/ExpenseItemObj.js
@@ -0,0 +1,15 @@
+const rand = () => {
+  return Math.floor(Math.random() * 9999 + 1000);
+};
+
+const expenseItemObj = (title, amount, date) => {
+  // Can not omit curly brackets when returning object literals
+  return {
+    id: rand(),
+    title: title,
+    amount: amount,
+    date: date,
+  };
+};
+
+export default expenseItemObj;
